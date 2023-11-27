a62ffb9fc6d89bdff78872227c12d6c5e547a5c7
Anaelle BARGAS <abargas@leonard-solutions.com>
2023-11-27 14:01:10 +0100

    balbl

---
+++ js/index2.js
@@ -0,0 +1,9 @@
+function rapetisser(i){
+    document.querySelectorAll("#centre_droite>div>div>img")[i].style.height="50%";
+    console.log("bla")
+}
+
+function agrandir(i){
+    document.querySelectorAll("#centre_droite>div>div>img")[i].style.height="60%";
+}
+
