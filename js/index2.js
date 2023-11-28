e785c28e6ff8732e222a35844ff94c4c1d582417
unknown <anaelle.bargas@sts-sio-caen.info>
2023-11-28 23:14:36 +0100

    blabla

--- js/index2.js
+++ js/index2.js
@@ -7,3 +7,25 @@ function agrandir(i){
     document.querySelectorAll("#centre_droite>div>div>img")[i].style.height="55%";
 }
 
+let dernierDeplacement = 0;
+
+window.addEventListener("scroll", function() {
+  const deplacementActuel = window.scrollY;
+
+  if (deplacementActuel > dernierDeplacement) {
+    // Défilement vers le bas
+    console.log("Défilement vers le bas");
+  } else if (deplacementActuel < dernierDeplacement) {
+    // Défilement vers le haut
+    // document.querySelector("::-webkit-scrollbar-thumb").style.background="linear-gradient(#213049, transparent);"
+    console.log("Défilement vers le haut");
+  }
+
+  dernierDeplacement = deplacementActuel;
+});
+
+
+function onglets_sur_le_cote(){
+    console.log("bla")
+    document.querySelector("#centre_droite>div>a").style.right="0.5%";
+}
