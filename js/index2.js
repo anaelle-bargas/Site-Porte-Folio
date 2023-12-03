aaadc3f5c9ac370453cbe8514dcb072c2dd5a0d7
unknown <anaelle.bargas@sts-sio-caen.info>
2023-12-03 13:21:39 +0100

    page d'accueil terminée

--- js/index2.js
+++ js/index2.js
@@ -9,7 +9,7 @@ function agrandir(i){
 
 let dernierDeplacement = 0;
 
-
+nbClick = 0;
 
 
 function onglets_sur_le_cote(){
@@ -19,5 +19,13 @@ function onglets_sur_le_cote(){
   document.querySelector("#centre_gauche").style.position="fixed";
   document.querySelector("#centre_droite").style.position="fixed";
   document.querySelector("#centre_droite").style.position="fixed";
-
+  nbClick++;
+  if(nbClick==1){
+    document.querySelector("#div_formations").classList.add('div_formations');
+    document.querySelector("#div_accueil").classList.add('div_accueil');
+    document.querySelector("#div_competences").classList.add('div_competences');
+    document.querySelector("#div_experience").classList.add('div_experience');
+    document.querySelector("#div_contact").classList.add('div_contact');
+    document.querySelector("#div_a_propos").classList.add('div_a_propos');
+  } 
 }
