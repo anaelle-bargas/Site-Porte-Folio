ddaf12153cd931d6776a9a59f16df5a1eea34b81
unknown <anaelle.bargas@sts-sio-caen.info>
2023-12-03 21:16:05 +0100

    acceuil finished

--- js/index2.js
+++ js/index2.js
@@ -24,8 +24,51 @@ function onglets_sur_le_cote(){
     document.querySelector("#div_formations").classList.add('div_formations');
     document.querySelector("#div_accueil").classList.add('div_accueil');
     document.querySelector("#div_competences").classList.add('div_competences');
-    document.querySelector("#div_experience").classList.add('div_experience');
+    document.querySelector("#div_experiences").classList.add('div_experiences');
     document.querySelector("#div_contact").classList.add('div_contact');
     document.querySelector("#div_a_propos").classList.add('div_a_propos');
   } 
 }
+
+// var div_actuelle = "premiere_vue";
+
+// function actuelle_div(id_div){
+//   document.querySelector("#div_"+id_div).style.transform="translateX(-30%);"
+// }
+
+function check_qui(){
+  
+  document.getElementById("radio_passionnee").checked=false;
+  document.getElementById("paroles").style.animation="deplacement_passionnee_qui 1s 0.0s ease-out forwards";
+}
+
+function check_passionnee(){
+  document.getElementById("radio_qui").checked=false;
+  document.getElementById("paroles").style.animation="deplacement_qui_passionnee 1s 0.0s ease-out forwards";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
